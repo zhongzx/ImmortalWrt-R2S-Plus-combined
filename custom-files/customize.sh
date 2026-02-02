6ab9daceba653c7e0c2671d8cb0853debd82c39c
Zhaoxin Zhong <zhong.zx@gmail.com>
2026-02-02 11:56:02 +0800

    Create customize.sh to modify default settings

---
+++ custom-files/customize.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# 修改默认 IP
+sed -i 's/192.168.1.1/192.169.2.1/g' package/base-files/files/bin/config_generate
+
+# 设置 root 默认密码为 password
+PASSWD=$(openssl passwd -1 "password")
+sed -i "s#root::0:0:99999:7:::#root:${PASSWD}:0:0:99999:7:::#" package/base-files/files/etc/shadow
+
+# 更改 banner（登录后台左上角、SSH 欢迎界面显示）
+sed -i "s/'OpenWrt'/'R2S Plus'/g" package/base-files/files/etc/banner
